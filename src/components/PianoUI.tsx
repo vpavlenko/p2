f01ab258b1a4cfd12f7e77a58cd57b6363390a66
Vitaly Pavlenko <cxielamiko@gmail.com>
2024-11-03 09:29:31 +0400

    make several octaves

--- src/components/PianoUI.tsx
+++ src/components/PianoUI.tsx
@@ -3,11 +3,12 @@ import * as React from "react";
 const BLACK_KEYS = [1, 3, -1, 6, 8, 10, -1];
 const WHITE_KEYS = [0, 2, 4, 5, 7, 9, 11];
 const BLACK_KEY_LABELS = ["♭2", "♭3", "", "♯4", "♭6", "♭7", ""];
+const NUM_OCTAVES = 7;
 
-const KEY_WIDTH = 40;
-const KEY_HEIGHT = 80;
-const ROW_DISTANCE = 50;
-const PADDING = 5;
+const KEY_WIDTH = 14;
+const KEY_HEIGHT = 40;
+const ROW_DISTANCE = KEY_HEIGHT * 0.5;
+const PADDING = 0;
 
 const COLORS: { [key: number]: string } = {
   0: "white",
@@ -35,10 +36,10 @@ const PianoKey: React.FC<{
     backgroundColor,
     position: "absolute" as const,
     userSelect: "none" as const,
-    fontSize: "20px",
+    fontSize: "10px",
     textAlign: "center" as const,
     color: "white",
-    textShadow: "0px 0px 5px black, 0px 0px 3px black",
+    textShadow: "0px 0px 3px black, 0px 0px 2px black",
     display: "grid",
     alignContent: "end",
     boxSizing: "border-box" as const,
@@ -46,7 +47,7 @@ const PianoKey: React.FC<{
 
   return (
     <div style={keyStyle}>
-      {label}
+      {/* {label} */}
       {note === 1 && (
         <div
           style={{
@@ -83,38 +84,38 @@ const PianoKey: React.FC<{
 
 export const PianoUI: React.FC = () => {
   return (
-    <div style={{ backgroundColor: "black", padding: "10px" }}>
+    <div style={{ backgroundColor: "black", padding: "5px" }}>
       <div
         style={{
           position: "relative",
-          width: WHITE_KEYS.length * (KEY_WIDTH + PADDING),
+          width: WHITE_KEYS.length * KEY_WIDTH * NUM_OCTAVES,
           height: KEY_HEIGHT + ROW_DISTANCE,
         }}
       >
-        {[0, 1, 2, 3, 4, 5, 6].map((i) => (
+        {Array.from({ length: WHITE_KEYS.length * NUM_OCTAVES }, (_, i) => (
           <React.Fragment key={i}>
             <PianoKey
-              note={WHITE_KEYS[i]}
-              label={(i + 1).toString()}
+              note={WHITE_KEYS[i % 7]}
+              label={((i % 7) + 1).toString()}
               style={{
                 top: ROW_DISTANCE,
-                left: (KEY_WIDTH + PADDING) * i,
+                left: KEY_WIDTH * i,
                 width: KEY_WIDTH,
                 height: KEY_HEIGHT,
-                borderRadius: "5px",
+                borderRadius: "3px",
               }}
             />
-            {BLACK_KEYS[i] !== -1 && (
+            {BLACK_KEYS[i % 7] !== -1 && (
               <PianoKey
-                note={BLACK_KEYS[i]}
-                label={BLACK_KEY_LABELS[i]}
+                note={BLACK_KEYS[i % 7]}
+                label={BLACK_KEY_LABELS[i % 7]}
                 style={{
                   top: 0,
-                  left: (KEY_WIDTH + PADDING) * (i + 0.5),
+                  left: KEY_WIDTH * (i + 0.5),
                   zIndex: 2,
                   width: KEY_WIDTH,
                   height: KEY_HEIGHT,
-                  borderRadius: "5px",
+                  borderRadius: "3px",
                 }}
               />
             )}
