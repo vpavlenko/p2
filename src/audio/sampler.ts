c95f62fd04283c1ac681491ae557741a357377fb
Vitaly Pavlenko <cxielamiko@gmail.com>
2024-11-22 13:48:31 +0500

    download salamander piano samples

--- src/audio/sampler.ts
+++ src/audio/sampler.ts
@@ -1,28 +1,45 @@
 import * as Tone from "tone";
 
+// Import all audio files
+import C2 from "../assets/salamander/C2.mp3";
+import Ds2 from "../assets/salamander/Ds2.mp3";
+import Fs2 from "../assets/salamander/Fs2.mp3";
+import A2 from "../assets/salamander/A2.mp3";
+import C3 from "../assets/salamander/C3.mp3";
+import Ds3 from "../assets/salamander/Ds3.mp3";
+import Fs3 from "../assets/salamander/Fs3.mp3";
+import A3 from "../assets/salamander/A3.mp3";
+import C4 from "../assets/salamander/C4.mp3";
+import Ds4 from "../assets/salamander/Ds4.mp3";
+import Fs4 from "../assets/salamander/Fs4.mp3";
+import A4 from "../assets/salamander/A4.mp3";
+import C5 from "../assets/salamander/C5.mp3";
+import Ds5 from "../assets/salamander/Ds5.mp3";
+import Fs5 from "../assets/salamander/Fs5.mp3";
+import A5 from "../assets/salamander/A5.mp3";
+
 let samplerLoaded = false;
 let loadingPromise: Promise<void> | null = null;
 
 export const sampler = new Tone.Sampler({
   urls: {
-    C2: "C2.mp3",
-    "D#2": "Ds2.mp3",
-    "F#2": "Fs2.mp3",
-    A2: "A2.mp3",
-    C3: "C3.mp3",
-    "D#3": "Ds3.mp3",
-    "F#3": "Fs3.mp3",
-    A3: "A3.mp3",
-    C4: "C4.mp3",
-    "D#4": "Ds4.mp3",
-    "F#4": "Fs4.mp3",
-    A4: "A4.mp3",
-    C5: "C5.mp3",
-    "D#5": "Ds5.mp3",
-    "F#5": "Fs5.mp3",
-    A5: "A5.mp3",
+    C2,
+    "D#2": Ds2,
+    "F#2": Fs2,
+    A2,
+    C3,
+    "D#3": Ds3,
+    "F#3": Fs3,
+    A3,
+    C4,
+    "D#4": Ds4,
+    "F#4": Fs4,
+    A4,
+    C5,
+    "D#5": Ds5,
+    "F#5": Fs5,
+    A5,
   },
-  baseUrl: "https://tonejs.github.io/audio/salamander/",
   onload: () => {
     console.log("Sampler loaded");
     console.log("Audio Context State:", Tone.getContext().state);
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/p2/", // Replace 'p2' with your repository name
+  base: "/p2/",
+  build: {
+    assetsDir: "assets",
+  },
 });
